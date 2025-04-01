ca1cd917d2350fbb2904ba81e7f3e01eb0a8dfdc
Qadir93 <abqadirkhan3@gmail.com>
2025-04-01 15:41:59 +0500

    Add files via upload

---
+++ Q-7/script.js
@@ -0,0 +1,12 @@
+var studentName;
+studentName = "Abdul Qadir Khan";
+
+var studentAge;
+studentAge = "29 years old";
+
+var studentCourse;
+studentCourse = "Web Development";
+
+alert("Student Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Course: " + studentCourse);
